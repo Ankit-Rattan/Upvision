d3e047289d49a42eb40e61a94f54a3526f4fe563
Ankit <rattanankit2004@gmail.com>
2024-04-20 13:13:03 +0530

    prefnl changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import React, { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import Footer from './components/Footer';
-import Team from './components/Team';
-import Home from './Home';
-import AllTeam from './components/AllTeam';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Loader from './components/Loader'
+import React, { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+import Team from "./components/Team";
+import Home from "./Home";
+import AllTeam from "./components/AllTeam";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -16,28 +15,27 @@ function App() {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    },5000);
+    }, 5000);
   }, []);
 
   return (
     <>
-      {loading ? 
+      {loading ? (
         <Loader />
-       : 
+      ) : (
         <div>
-          <BrowserRouter> 
+          <BrowserRouter>
             <div>
               <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/allteam' element={<AllTeam />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/allteam" element={<AllTeam />} />
               </Routes>
               <Footer />
             </div>
           </BrowserRouter>
         </div>
-      }
+      )}
     </>
->>>>>>> eddd3888af0e7c6c805135d18438f087f4084ee2
   );
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Footer from './components/Footer'
-import Team from './components/Team'
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Hero from './src/Hero'
-import EventPage from './src/EventPage'
-import GalleryPage from './src/GalleryPage'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+import Team from "./components/Team";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Hero from "./components/Hero";
+import EventPage from "./components/EventPage";
+import GalleryPage from "./components/GalleryPage";
+import Navbar from "./components/Navbar";
+import Message from "./components/Message";
 
 // import TeamCard from './components/GS'
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Hero />
+      <Message />
       <EventPage />
       <GalleryPage />
       <Team />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
