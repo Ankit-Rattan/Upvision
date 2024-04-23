b9b15857a11e55b785bb1ab85a763c9b1821c053
Ankit <rattanankit2004@gmail.com>
2024-04-24 01:00:56 +0530

    fnl chanegs

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,7 @@ import AllTeam from "./components/AllTeam";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "./components/Loader";
 import Alumuni from "./components/Alumuni";
-<<<<<<< HEAD
 import FullGalleryPage from "./components/FullGalleryPage";
-=======
->>>>>>> 5e952d39a66e42841b4eb1cdb42c8c5766b26b3c
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -36,12 +33,8 @@ function App() {
                 <Route path="/allteam" element={<AllTeam />} />
                 <Route path="/Contact-Us" element={<Footer />} />
                 <Route path="/allteam" element={<AllTeam />} />
-<<<<<<< HEAD
                 <Route path="/alumni" element={<Alumuni />} />
                 <Route path="/fullgallery" element={<FullGalleryPage/>} />
-=======
-                <Route path="/alumuni" element={<Alumuni />} />
->>>>>>> 5e952d39a66e42841b4eb1cdb42c8c5766b26b3c
               </Routes>
               <Footer />
             </div>
--- src/components/Alumuni.jsx
+++ src/components/Alumuni.jsx
@@ -5,74 +5,97 @@ import GS from "../images/GS.jpg";
 import "../Styles/Footer.css";
 import Navbar from "./Navbar.jsx";
 
-const Alumuni = () => {
+const Alumni = () => {
   return (
     <>
-      <Navbar/>
-      <div className="bg-zinc-800 pb-5  px-[3rem] max-w-[100%] ">
-          <p className="text-slate-200 text-center typing2 mb-3">Our Alumni</p>
-        <div className="" >
-const Team = () => {
-  return (
-    <>
-      <div className="bg-zinc-800 pb-5  px-[3rem] max-w-[100%] ">
-        <div className="" >
-          <p className="text-slate-200 text-center typing2 mb-3">Our Alumuni</p>
-          <div className="allteam">
+      <Navbar />
+      <div className="bg-zinc-800 pb-5 px-[1rem] max-w-[100%]">
+        <p className="text-slate-200 text-center typing2 mb-3">Our Alumni</p>
+        <div className="">
+          <div className="bg-zinc-800 pb-5 px-[1rem] max-w-[100%]">
             <div className="">
-              <div className="">
-                <div className="card flex justify-evenly flex-wrap mx-[5rem]">
-                  <div>
-                    <DGS
-                      name="Navneet Singh"
-                      desig="General Secretary (2022-2023)"
-                      img="https://media.licdn.com/dms/image/D4D03AQGbWZbg7-DjVg/profile-displayphoto-shrink_400_400/0/1669055975082?e=1719446400&v=beta&t=HlIhpcl-TBBIo7tXws4FKCnWcH926EZQWebibW3wov4"
-                      linkedin="https://www.linkedin.com/in/navneetsingh123/"
-                      email="mailto:linkedin.com/in/navneetsingh123"
-                    />
-                  </div>
-                  <div>
-                    <DGS
-                      name="Rahul Sinha"
-                      desig="General Secretary (2021-2022)"
-                      img="https://media.licdn.com/dms/image/D4D03AQFM5Hf0qzvOMw/profile-displayphoto-shrink_400_400/0/1694754193166?e=1719446400&v=beta&t=kLVZmuIisoArINBdPdgmA5EUpMwl0rkKIHAbIY3A1Q4"
-                      linkedin="https://www.linkedin.com/in/rahulsinha19/"
-                      email="mailto:#"
-                    />
-                  </div>
-
-                </div>
-                <hr className="my-[3rem]" />
-                <div className="flex justify-evenly flex-wrap mx-[5rem]">
-                  <div>
-                  <DGS
-                      name="Vaishnv Raju"
-                      desig="Deputy General Secretary (2022-2023)"
-                      img={GS}
-                      linkedin="https://www.linkedin.com/in/vaishnv-raju-9j/"
-                      email="mailto:techclub@nitdelhi.ac.in"
-                    />
-                  </div>
-                  <div>
-                    <DGS
-                      name="Aryan shrivastava"
-                      desig="Deputy Secretary General (2022-2023)"
-                      img="https://media.licdn.com/dms/image/C4E03AQHnxbnWlra-Bw/profile-displayphoto-shrink_400_400/0/1617206667667?e=1719446400&v=beta&t=ekat9--hYJ3gPubIvNuMsjNaD7ctSNN3DVNoFRIu72s"
-                      linkedin="https://www.linkedin.com/in/aryan-srivastava-17787b201/"
-                      email="mailto:srivaryan2@gmail.com"
-                    />
+              <div className="allteam">
+                <div className="">
+                  <div className="">
+                    <div className="card flex justify-evenly flex-wrap  gap-[.5rem]">
+                      <div>
+                        <DGS
+                          name="Vaishnv Raju"
+                          desig="General Secretary (2023-2024)"
+                          img={GS}
+                          linkedin="https://www.linkedin.com/in/vaishnv-raju-9j/"
+                          email="mailto:vaishnv@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Navneet Singh"
+                          desig="General Secretary (2022-2023)"
+                          img="https://media.licdn.com/dms/image/D4D03AQGbWZbg7-DjVg/profile-displayphoto-shrink_400_400/0/1669055975082?e=1719446400&v=beta&t=HlIhpcl-TBBIo7tXws4FKCnWcH926EZQWebibW3wov4"
+                          linkedin="https://www.linkedin.com/in/navneetsingh123/"
+                          email="mailto:navneetsingh@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Rahul Sinha"
+                          desig="General Secretary (2021-2022)"
+                          img="https://media.licdn.com/dms/image/D4D03AQFM5Hf0qzvOMw/profile-displayphoto-shrink_400_400/0/1694754193166?e=1719446400&v=beta&t=kLVZmuIisoArINBdPdgmA5EUpMwl0rkKIHAbIY3A1Q4"
+                          linkedin="https://www.linkedin.com/in/rahulsinha19/"
+                          email="mailto:rahulsinha@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Aditya Sharma"
+                          desig="General Secretary (202-2021)"
+                          img="https://media.licdn.com/dms/image/D4D03AQErmwCsCtX7Sw/profile-displayphoto-shrink_400_400/0/1695185770687?e=1719446400&v=beta&t=8WgKpN9xjikqjFLgDcC6xxapYemJUBEtDXjez9Ullio"
+                          linkedin="https://www.linkedin.com/in/aditya-sharma-6b18ba158/"
+                          email="mailto:rahulsinha@example.com"
+                        />
+                      </div>
                     </div>
-                    <div>
-
-                    <DGS
-                      name="Abhishek Kumar Suman"
-                      desig="Deputy Secretary General (2021-2022)"
-                      img="https://media.licdn.com/dms/image/D4D03AQHMprxBOHeMRg/profile-displayphoto-shrink_400_400/0/1686179263863?e=1719446400&v=beta&t=q50YpIagDWvW8Mlo2ncvCj3Ju6ETXxOeBQ10RwgZDiA"
-                      linkedin="https://www.linkedin.com/in/aksuman055/"
-                      email="https://www.linkedin.com/in/aksuman055/"
-                    />
+                    <hr className="my-[3rem]" />
+                    <div className="flex justify-evenly flex-wrap mx-[3rem] gap-[0.5rem]">
+                      <div>
+                        <DGS
+                          name="Vaishnv Raju"
+                          desig="Deputy General Secretary (2022-2023)"
+                          img={GS}
+                          linkedin="https://www.linkedin.com/in/vaishnv-raju-9j/"
+                          email="mailto:vaishnv@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Aryan shrivastava"
+                          desig="Deputy Secretary General (2022-2023)"
+                          img="https://media.licdn.com/dms/image/C4E03AQHnxbnWlra-Bw/profile-displayphoto-shrink_400_400/0/1617206667667?e=1719446400&v=beta&t=ekat9--hYJ3gPubIvNuMsjNaD7ctSNN3DVNoFRIu72s"
+                          linkedin="https://www.linkedin.com/in/aryan-srivastava-17787b201/"
+                          email="mailto:aryans@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Abhishek Kumar Suman"
+                          desig="Deputy Secretary General (2021-2022)"
+                          img="https://media.licdn.com/dms/image/D4D03AQHMprxBOHeMRg/profile-displayphoto-shrink_400_400/0/1686179263863?e=1719446400&v=beta&t=q50YpIagDWvW8Mlo2ncvCj3Ju6ETXxOeBQ10RwgZDiA"
+                          linkedin="https://www.linkedin.com/in/aksuman055/"
+                          email="mailto:aksuman@example.com"
+                        />
+                      </div>
+                      <div>
+                        <DGS
+                          name="Rahul Sinha"
+                          desig="Deputy General Secretary (2020-2021)"
+                          img="https://media.licdn.com/dms/image/D4D03AQFM5Hf0qzvOMw/profile-displayphoto-shrink_400_400/0/1694754193166?e=1719446400&v=beta&t=kLVZmuIisoArINBdPdgmA5EUpMwl0rkKIHAbIY3A1Q4"
+                          linkedin="https://www.linkedin.com/in/rahulsinha19/"
+                          email="mailto:rahulsinha@example.com"
+                        />
+                      </div>
+                    </div>
+ 
                   </div>
-
                 </div>
               </div>
             </div>
@@ -83,8 +106,4 @@ const Team = () => {
   );
 };
 
-<<<<<<< HEAD
-export default Alumuni  ;
-=======
-export default Team;
->>>>>>> 5e952d39a66e42841b4eb1cdb42c8c5766b26b3c
+export default Alumni;
